0e898b313ff668aa15765cf3923d5963d384d0bf
hicka04 <hicka04@gmail.com>
2020-04-24 22:49:46 +0900

    Board

---
+++ Reversi/Domain/Board.swift
@@ -0,0 +1,16 @@
+//
+//  Board.swift
+//  Reversi
+//
+//  Created by hicka04 on 2020/04/24.
+//  Copyright © 2020 Yuta Koshizawa. All rights reserved.
+//
+
+import Foundation
+
+struct Board {
+    let width = 8
+    let height = 8
+    
+    var cells: [[Cell]]
+}
